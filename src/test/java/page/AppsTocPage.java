8686676cf9dd9aa711b6659075dc0fa89303f8c4
vita152 <vitakosmyna@gmail.com>
2017-07-18 06:43:34 +0300

    TimeStamps is unique

---
+++ src/test/java/page/AppsTocPage.java
@@ -0,0 +1,7 @@
+package page;
+
+/**
+ * Created by Вита on 18.07.2017.
+ */
+public class AppsTocPage {
+}
---
+++ src/test/java/test/AppsTocTest.java
@@ -0,0 +1,7 @@
+package test;
+
+/**
+ * Created by Вита on 18.07.2017.
+ */
+public class AppsTocTest {
+}
